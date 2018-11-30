12982fe0e0ec363207e572b84f6751798910932b
Steven G. Harms <steven.harms@flatironschool.com>
2018-11-30 14:04:20 -0500

    Add solution

--- demo.js
+++ demo.js
@@ -1,8 +1,32 @@
-// JavaScript code goes here!
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
 
+let articleHearts = document.querySelectorAll(".like");
 
-// Your code goes above this line!
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall("bogusUrl")
+   //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      alert("Something went wrong!");
+    });
+}
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
